401b3f5beea5b570bf224af689a8e628f0644197
Orezimena Okudhuru <emmanuel.o.okudhuru@gmail.com>
2023-02-15 00:35:07 +0100

    Init requirements

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL_1")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
